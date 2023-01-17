2ec84508ff802f3a3f46fd7a0c11527c3d8cea66
dvlpr1996 <nimajahanbakhshian@gmail.com>
2023-01-18 00:29:45 +0330

    install and config tailwindCss;

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+		'./storage/framework/views/*.php',
+		'./resources/views/**/*.blade.php',
+	],
+
+	theme: {
+		screens: {
+			'xs': '360px',
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1536px',
+		},
+		extend: {},
+	},
+
+	plugins: [
+		require('tailwindcss-debug-screens'),
+	],
+};
